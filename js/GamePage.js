2c9995f2982edf714354bc9dc16db13099e32610
“Amaal <amal.asaad26@gmail.com>
2021-01-07 21:13:34 +0200

    Add sound effects

--- js/GamePage.js
+++ js/GamePage.js
@@ -5,7 +5,7 @@ $(".ready").toggle(1000);
 $(".go").show(1000).slideUp(3000);
 $("#pauseBtn").click();
 $("pause").toggle(1000);
-$(".playSound").
+
 
 document.getElementById("pauseBtn").addEventListener("click",popUp);
 document.getElementById("close").addEventListener("click",popUp);
@@ -31,4 +31,45 @@ function backHome(){
 document.getElementById("reset").addEventListener("click",runGame);
 
 
+var sound = document.getElementById("audio");
+var music = document.getElementById("audioMusic");
+document.getElementById("playSound").addEventListener("click",playSound);
+document.getElementById("playMusic").addEventListener("click",playMusic);
 
+var off1 = document.getElementById("off1");
+var off2 = document.getElementById("off2");
+var offtxt1 = document.getElementById("offtxt1");
+var offtxt2 = document.getElementById("offtxt2");
+
+
+var count = 0;
+function playSound(){
+        if(count === 0){
+           count = 1;
+           sound.play();
+           offtxt1.textContent = "ON";
+        //    off1.setAttribute("src","../Img/on.png");
+           
+        }
+        else{
+            sound.pause();
+            count = 0;
+            offtxt1.textContent = "OFF";
+            // off1.setAttribute("src","../Img/off.png");
+        }
+}
+
+
+var countMusic = 0;
+function playMusic(){
+    if(countMusic === 0){
+       countMusic = 1;
+       music.play();
+       offtxt2.textContent = "ON";
+    }
+    else{
+        music.pause();
+        countMusic = 0;
+        offtxt2.textContent = "OFF";
+    }
+}
