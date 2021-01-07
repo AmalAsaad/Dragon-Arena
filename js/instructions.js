d01e03328d313ffd2f3497f149a21713fedfafa4
Mohamed Ahmed <mohamedahmedali1295@gmail.com>
2021-01-07 04:15:07 -0800

    how to play (instruction)

---
+++ js/instructions.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+$("#box h3").text("HOW TO PLAY")
+$("<div>").appendTo("#box").attr("id","firstpagemove")
+$("<div>").appendTo("#box").attr("id","firstpagefire")
+//$("<p>").appendTo("#firstpagemove").text("use arrow to move")
