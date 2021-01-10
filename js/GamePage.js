f01ad03a5e2d2308e2ff0e8415d7fb6d216a93b9
“Amaal <amal.asaad26@gmail.com>
2021-01-10 10:06:04 +0200

    add sound animation, edit MusicSound effect

--- js/GamePage.js
+++ js/GamePage.js
@@ -4,27 +4,11 @@
 
 // =====================================================//
 function runGame(){
+    if(countMusic === 1){
+        music.play();
+    }
     location.assign("gamePage.html");
-    // myGameArea.start();
 }
-// var myGameArea = {
-//     canvas : document.createElement("canvas"),
-//     start : function() {
-//         this.canvas.width = 1279;
-//         this.canvas.height = 565;
-//         this.context = this.canvas.getContext("2d");
-//         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-//         this.interval = setInterval(updateGameArea, 20);
-//     },
-//     clear : function() {
-//         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-//     }
-// }
-// function updateGameArea() {
-//     myGameArea.clear();
-//     myGamePiece.update();
-// }
-
 
 
 //Ready go show up//
@@ -38,6 +22,9 @@ document.getElementById("pauseBtn").addEventListener("click",popUp);
 document.getElementById("close").addEventListener("click",popUp);
 var click = 0;
 function popUp(){
+    if(countMusic === 1){
+        music.play();
+    }
     if(click === 0){
         document.getElementById("container4").style.display="block";
         click = 1;
@@ -48,11 +35,17 @@ function popUp(){
 }
 document.getElementById("cont").addEventListener("click",contPlay);
 function contPlay(){
-    document.getElementById("container4").style.display="none";
+    if(countMusic === 1){
+        music.play();
+    }
+    document.getElementById("container4").style.display="none"; 
 }
 
 document.getElementById("homePage").addEventListener("click",backHome);
 function backHome(){
+    if(countMusic === 1){
+        music.play();
+    }
     location.assign("index.html");
 }
 document.getElementById("reset").addEventListener("click",runGame);
@@ -72,30 +65,43 @@ var offtxt2 = document.getElementById("offtxt2");
 var count = 0;
 function playSound(){
         if(count === 0){
-           off1.src = "Img/on.png";
            count = 1;
-           sound.play();
-           offtxt1.textContent = "ON";
+           sound.play();        
+           $("#playSound").animate({left :'85px'},200, function(){
+                off1.src = "Img/on.png";
+                offtxt1.textContent = "ON";
+                $("#offtxt1").css("left","55px");                
+           });
         }
         else{
-            off1.src = "Img/off.png";
             sound.pause();
             count = 0;
-            offtxt1.textContent = "OFF";           
+            $("#playSound").animate({left :'30px'},200, function(){
+                off1.src = "Img/off.png";
+                $("#offtxt1").css("left","90px");
+                offtxt1.textContent = "OFF";    
+           });
+                      
         }
 }
 var countMusic = 0;
 function playMusic(){
     if(countMusic === 0){
-       off2.src = "Img/on.png";
        countMusic = 1;
        music.play();
-       offtxt2.textContent = "ON";
+       $("#playMusic").animate({left :'85px'},200, function(){
+        off2.src = "Img/on.png";
+        offtxt2.textContent = "ON";
+        $("#offtxt2").css("left","55px");                
+   });
     }
     else{
-        off2.src = "Img/off.png";
         music.pause();
         countMusic = 0;
-        offtxt2.textContent = "OFF";
+        $("#playMusic").animate({left :'30px'},200, function(){
+            off2.src = "Img/off.png";
+            $("#offtxt2").css("left","90px");
+            offtxt2.textContent = "OFF";    
+       });
     }
 }
