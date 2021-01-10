dcb5834a777a69a3ddd57f77f5b301090a2ab3c8
samarkhaled4 <samarkhaled.iti@gmail.com>
2021-01-10 04:06:08 +0200

    Editing owner menu displaying

--- js/HomePage.js
+++ js/HomePage.js
@@ -3,17 +3,24 @@ function runGame(){
     location.assign("gamePage.html");
 }
 ////////Owner button////////////
-var ownerbtn=document.getElementById("owner");
-ownerbtn.addEventListener("click",fun1)
-var list=document.getElementById("names")
-function fun1(){ 
-       list.style.display='block'
-}
-var exitbtn=document.getElementById("exit")
-exitbtn.addEventListener("click",fun2)
-function fun2(){
-    list.style.display='none'
-}
+$("#owner").click(function(){
+    $("#names").slideToggle();
+});
+// $("#exit").click(()=>{
+//     $("#names").slideUp();
+// })
+///////////////////////////////
+// var ownerbtn=document.getElementById("owner");
+// ownerbtn.addEventListener("click",fun1)
+// var list=document.getElementById("names")
+// function fun1(){ 
+//        list.style.display='block'
+// }
+// var exitbtn=document.getElementById("exit")
+// exitbtn.addEventListener("click",fun2)
+// function fun2(){
+//     list.style.display='none'
+// }
 ////////bouncing play button//////////
 // $("#play").effect( "bounce", {times:3}, 300 );
 //////////////Insruction button///////////////////
